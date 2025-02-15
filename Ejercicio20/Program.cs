a8e73bafa70b0d73ac005e72802e89c361c907c0
Dogor0555 <74686946+Dogor0555@users.noreply.github.com>
2025-02-15 06:34:44 -0600

    Ejercicio20

--- Ejercicio20/Program.cs
+++ Ejercicio20/Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+class Program
+{
+    static double AreaRectangulo(double baseRectangulo, double altura)
+    {
+        return baseRectangulo * altura;
+    }
+
+    static void Main(string[] args)
+    {
+        Console.Write("Ingrese la base del rectángulo: ");
+        double baseRectangulo = double.Parse(Console.ReadLine());
+
+        Console.Write("Ingrese la altura del rectángulo: ");
+        double altura = double.Parse(Console.ReadLine());
+
+        double area = AreaRectangulo(baseRectangulo, altura);
+        Console.WriteLine("El área del rectángulo es: " + area);
+    }
+}
